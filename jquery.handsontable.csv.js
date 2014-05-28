6b37702e1f5cd11118984f883dd1ddb26216a4fd
Juan Tascon <juantascon@horlux.org>
2014-05-28 11:04:45 -0500

    added initial version

---
+++ jquery.handsontable.csv.js
@@ -0,0 +1,32 @@
+var handsontable2csv = {
+    string: function(instance) {
+        var headers = instance.getColHeader();
+        
+        var csv = headers.join(";") + "\n";
+        
+        for (var i = 0; i < instance.countRows(); i++) {
+            var row = [];
+            for (var h in headers) {
+                var prop = instance.colToProp(h)
+                var value = instance.getDataAtRowProp(i, prop)
+                row.push(value)
+            }
+            
+            csv += row.join(";")
+            csv += "\n";
+        }
+        
+        return csv;
+    },
+    
+    download: function(instance, filename) {
+        var csv = handsontable2csv.string(instance)
+        console.log(csv)
+        var link = document.createElement("a");
+        link.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(csv));
+        link.setAttribute("download", filename);
+        document.body.appendChild(link)
+        link.click();
+        document.body.removeChild(link)
+    }
+}
